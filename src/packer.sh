5bcba428332120c9f32a9e18649a5a7fd8ce2e28
Vegar Andreas Bergum <vegar.andreas@gmail.com>
2018-12-25 15:47:59 +0100

    functioning pywal and packer scripts

--- src/packer.sh
+++ src/packer.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
-pacman -S fakeroot jshon expac git wget --noconfirm -Sy
+sudo pacman -S fakeroot jshon expac git wget --noconfirm
 
 cd /tmp
 wget https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=packer
 mv PKGBUILD\?h\=packer PKGBUILD
 makepkg
-pacman -U packer-*.pkg.tar.xz --noconfirm
-
+sudo pacman -U packer-*.pkg.tar.xz --noconfirm
--- src/pywal-fix.sh
+++ src/pywal-fix.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
-
-pacman -Rs python-pywal --noconfirm
-pacman -Sy python-pip --noconfirm
-pip install pywal==3.1.0
+sudo pacman -Rs python-pywal --noconfirm
+sudo pacman -Sy python-pip --noconfirm
+sudo pip install pywal==3.1.0
 
 NAME=$(whoami)
+firefox -CreateProfile 
+
+if [ ! -f "/home/$NAME/dotfiles/userChrome.css" ]; then
+	cd /home/$NAME/dotfiles/
+	wget https://raw.githubusercontent.com/vegarab/dotfiles/master/userChrome.css
+fi
+
 cd /home/$NAME/.mozilla/firefox/*.default/
 
 if [ ! -d "chrome" ]; then
 	mkdir chrome
 fi
 
-cp /home/$NAME/dotfiles/userChrome.CSS chrome/
+P1=$(pwd)
+sudo ln -s /home/$NAME/dotfiles/userChrome.css $P1/chrome/userChrome.css
+
 packer -S oomox --noconfirm
