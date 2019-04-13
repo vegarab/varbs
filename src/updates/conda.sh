653ffd1ffbdccf511ce932028bf693c9b32b2b27
Vegar Andreas Bergum <vegarab@stud.ntnu.no>
2019-04-13 15:46:15 +0200

    Fixes conda symlink error

--- src/updates/conda.sh
+++ src/updates/conda.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 packer -S python-conda --noconfirm
 mkdir -p /usr/etc/profile.d/
-sudo ln -s /etc/profile.d/conda.sh /usr/etc/profile.d/conda.sh
+sudo ln -s /usr/etc/profile.d/conda.sh /etc/profile.d/conda.sh 
 conda activate
